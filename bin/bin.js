5a3e0d4b3b60a748421ff978fe4e66e0d13f32cf
Tharaka Romesh <tharakaromesh@gmail.com>
2019-05-28 23:17:17 +0530

    add basic packages for cli

--- bin/bin.js
+++ bin/bin.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+const program = require("commander");
+const inquirer = require("inquirer");
+const figlet = require("figlet");
+const chalk = require("chalk");
+const emoji = require("node-emoji");
+
+program.option("-h, --help", "Help command for all options");
+program.parse(process.argv);
+
+if (program.help) console.log(`help is on the way ${emoji.get("coffee")}`);
