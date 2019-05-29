4c73c3dbf8a5bd97bcb0ebbc33bbeafe46bc6747
Tharaka Romesh <tharakaromesh@gmail.com>
2019-05-29 23:56:10 +0530

    add about section

--- bin/bin.js
+++ bin/bin.js
@@ -6,7 +6,52 @@ const figlet = require("figlet");
 const chalk = require("chalk");
 const emoji = require("node-emoji");
 
-program.option("-h, --help", "Help command for all options");
-program.parse(process.argv);
+program
+  .version("0.0.1", "-v, --version")
+  .option("-a, --about", "View about section of  NodeCloud CLI")
+  .option("-i, --init", "Add bbq sauce")
+  .parse(process.argv);
 
-if (program.help) console.log(`help is on the way ${emoji.get("coffee")}`);
+if (program.about) {
+  figlet.text(
+    "NodeCloud CLI",
+    {
+      font: "slant",
+      horizontalLayout: "default",
+      verticalLayout: "default"
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      console.log(chalk.blue(data));
+      console.log(
+        chalk.green("Sustainable Computing Research Group") +
+          chalk.blue(" (SCoRe)")
+      );
+    }
+  );
+}
+
+if (program.init) {
+  function doSomething(answers) {
+    // Do whateva you want!
+  }
+  var questions = [
+    {
+      message: "Select cloud service provider",
+      type: "list",
+      name: "providers",
+      choices: ["AWS", "GCP", "Azure", "Ali", "DigitalOcean"]
+    },
+    {
+      message: "Select region",
+      type: "list",
+      name: "providers",
+      choices: ["AWS", "GCP", "Azure", "Ali", "DigitalOcean"]
+    }
+  ];
+  inquirer.prompt(questions, doSomething);
+}
