460f1a4328b9a6fa3f735008c94d8520dc337279
Tharaka Romesh <tharakaromesh@gmail.com>
2019-07-28 21:36:27 +0530

    #10 Update README with commands

--- test/network.test.js
+++ test/network.test.js
@@ -1,1 +1,5 @@
-describe("Network Services", () => {});
+describe("Network Services", () => {
+  test("Create database", () => {
+    expect({ one: 1, two: 2 }).toEqual({ one: 1, two: 2 });
+  });
+});
