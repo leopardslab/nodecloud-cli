4e9d829ca3ac216a7dda590ecc371113b6eefacc
duc0905 <duc20022009@gmail.com>
2019-12-05 13:00:48 +0700

    add Github URL to version flag

--- bin/nc.js
+++ bin/nc.js
@@ -2,11 +2,16 @@
 const program = require("commander");
 const Services = require("../lib/services");
 const ora = require("ora");
-
+const package = require("../package.json");
 const spinner = ora();
 
 program
-  .version("0.0.1", "-v, --version")
+  .version(
+    `
+    Version: ${package.version}
+    Github: ${package.repository}`,
+    "-v, --version"
+  )
   .option("-a, --about", "View about section of NodeCloud CLI")
   .option("-i, --init", "Initialize provider")
   .option("-t, --type <type>", "Type")
