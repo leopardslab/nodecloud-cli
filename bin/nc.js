1aa6e3c5fb832e2e4e3817b5f79283bc878933ef
Joseph Semrai <josephsemrai@gmail.com>
2019-12-13 11:03:02 -0500

    Add version and repository info

--- bin/nc.js
+++ bin/nc.js
@@ -2,11 +2,20 @@
 const program = require("commander");
 const Services = require("../lib/services");
 const ora = require("ora");
+const packageJson = require("../package.json");
 
 const spinner = ora();
 
 program
-  .version("0.0.1", "-v, --version")
+  .version(
+    `
+  Project Information
+  ---
+  Repository (GitHub): ${packageJson.repository}
+  Nodecloud Version: ${packageJson.version}
+  `,
+    "-v, --version"
+  )
   .option("-a, --about", "View about section of  NodeCloud CLI")
   .option("-i, --init", "Initialize provider")
   .option("-t, --type <type>", "Type")
