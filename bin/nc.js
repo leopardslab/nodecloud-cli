738e790584da11ecb84dce42e1a95aec23426443
kunalbhatia <kunal18042002@gmail.com>
2020-01-07 11:03:33 +0530

    Update nc.js

--- bin/nc.js
+++ bin/nc.js
@@ -25,6 +25,7 @@ program
   .option("-h, --id <type>", "Id")
   .option("-m, --image <type>,", "Image")
   .option("-n, --instance <type>,", "Instance")
+  .option("--table", "Print JSON data in table format")
   .parse(process.argv);
 
 Services(program, spinner);
